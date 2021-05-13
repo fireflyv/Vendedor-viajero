14dfb4721afaffa1ab08566a49d2ce2495ab3cb6
ShepComm <constanza.castro@alumnos.ucn.cl>
2021-05-13 15:50:40 -0400

    v1 nodo

---
+++ Nodo.py
@@ -0,0 +1,27 @@
+"""
+nombre string
+padre nmodo
+hijos nodo[]
+nodo(nombre string)
+gets set
+"""
+class Nodo:
+
+
+    def __init__(self, nombre):
+        self.nombre= nombre
+        self.padre= None
+        self.hijos= [] #quede con duda en esta 
+
+    def getNombre(self):
+        return self.nombre
+    
+    def getPadre(self):
+        return self.padre
+    
+    def setNombre(self, newNombre):
+        self.nombre= newNombre
+    
+    def setPadre(self, newPadre):
+        self.padre= newPadre
+    
