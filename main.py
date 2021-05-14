e84da34cf410df2056f70a970340cb38544b86bf
Vale <valentina.fuentes@alumnos.ucn.cl>
2021-05-13 21:15:38 -0400

    Se agrago la funcion para graficar

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 #crea la heuristica del problema en una matriz de nxn
 def crearHeuristica(n):
     #los valores de la heuristica son al azar del 0 al 100
-    heuristica = np.random.random_integers(0, 100, (ciudades,ciudades))
+    heuristica = np.random.randint(0, 100, (ciudades,ciudades))
     # se reemplaza la diagonal por 0s
     np.fill_diagonal(heuristica,0)
     for i in range(len(heuristica)):
@@ -12,12 +13,30 @@ def crearHeuristica(n):
             if i > j :
                 heuristica[i][j] = heuristica[j][i]
     return heuristica
+# grafica cantidad de ciudades vs tiempo[ms] y lo guarda en plots
+def graficar(x,y):
+    #tiempo = y
+    #ciudades =x
+    plt.plot(x, y)
+    plt.xlabel("Cantidad de ciudades")
+    plt.ylabel("Tiempo [ms]")
+    plt.title("Ciudades vs tiempo")
+    plt.xticks(x)
+    plt.savefig(f"plots/grafico{len(x)}.png")
 
-ciudades = 2
+ciudades = 4
+x= []
+y =[]
 while True:
+    #Se calcula la heuristica
     heuristica = crearHeuristica(ciudades)
-    print(heuristica)
+    #se resulve el problema
+    #se guardan los resultados
+    x.append(ciudades)
+    y.append(np.random.randint(0, 100))
+    #se grafican
+    graficar(x,y)
     #las ciudades se incrementan en 2
     ciudades += 2
-    if ciudades == 6:
+    if ciudades == 10:
         break
